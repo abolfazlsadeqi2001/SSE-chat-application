ae7253787698c69978a3261baab3cce957eb7d79
abolfazlsadeqi2001 <abolfazlsadeqi2001@chmail.ir>
2020-03-19 12:08:25 +0330

    add get and set method for using mariadb

---
+++ chat-application/src/main/java/main/generals/database/maridb/MariaDBConnector.java
@@ -0,0 +1,27 @@
+package main.generals.database.maridb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import main.generals.config.reader.ConfigReader;
+
+public class MariaDBConnector {
+	private static final String CONNECTION_ADDRESS = "jdbc:mariadb://localhost:3306/chat";
+	private static final String USER = ConfigReader.getDatabaseUserName();
+	private static final String PASS = ConfigReader.getDatabasePassword();
+	
+	public static void set(String query) throws Exception {
+		Connection con = DriverManager.getConnection(CONNECTION_ADDRESS,USER,PASS);
+		Statement st = con.createStatement();
+		st.executeUpdate(query);
+	}
+	
+	public static ResultSet get(String query) throws Exception {
+		Connection con = DriverManager.getConnection(CONNECTION_ADDRESS,USER,PASS);
+		Statement st = con.createStatement();
+		ResultSet set = st.executeQuery(query);
+		return set;
+	}
+}
