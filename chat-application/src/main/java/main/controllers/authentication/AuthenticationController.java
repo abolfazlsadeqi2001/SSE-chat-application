312a57bbd22ff6689b251f85761b10b3823c3486
abolfazlsadeqi2001 <abolfazlsadeqi2001@chmail.ir>
2020-03-20 10:30:51 +0330

    add /register path

--- chat-application/src/main/java/main/controllers/authentication/AuthenticationController.java
+++ chat-application/src/main/java/main/controllers/authentication/AuthenticationController.java
@@ -48,4 +48,32 @@ public class AuthenticationController {
 		}
 	}
 	
+	@RequestMapping("/register")
+	public String register(@RequestParam(name="username",required=true) String userName,
+			@RequestParam(name="password",required=true) String password,
+			HttpServletResponse res,
+			Model model) {
+		try {
+			User user = new User();
+			user.setUserName(userName);
+			user.setPassword(password);
+			
+			Authenticator.register(user);
+			
+			Cookie userNameCookie = new Cookie(USER_NAME_COOKKIE_NAME,user.getUserName());
+			Cookie passwordCookie = new Cookie(PASSWORD_COOKKIE_NAME,user.getPassword());
+			
+			userNameCookie.setMaxAge(COOKIES_LIFE_TIME);
+			passwordCookie.setMaxAge(COOKIES_LIFE_TIME);
+			
+			res.addCookie(userNameCookie);
+			res.addCookie(passwordCookie);
+			
+			return CHAT_ROOM_PATH;
+		}catch(Exception e) {
+			model.addAttribute("error_message", e.getMessage());
+			return AUTHENTICATION_PATH;
+		}
+	}
+	
 }
