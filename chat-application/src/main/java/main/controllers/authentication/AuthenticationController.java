2bd31fc395de1472e418845a60d9c0ade9fa22dc
abolfazlsadeqi2001 <abolfazlsadeqi2001@chmail.ir>
2020-03-20 10:29:31 +0330

    add /login path

---
+++ chat-application/src/main/java/main/controllers/authentication/AuthenticationController.java
@@ -0,0 +1,51 @@
+package main.controllers.authentication;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import main.generals.authentication.Authenticator;
+import main.generals.authentication.models.User;
+
+@Controller
+public class AuthenticationController {
+	
+	private static final String AUTHENTICATION_PATH = "/authentication";
+	private static final String CHAT_ROOM_PATH = "/chat";
+	private static final String USER_NAME_COOKKIE_NAME = "username";
+	private static final String PASSWORD_COOKKIE_NAME = "password";
+	private static final int COOKIES_LIFE_TIME = 4 * 24 * 60 * 60;
+	
+	@RequestMapping("/login")
+	public String login(@RequestParam(name="username",required=true) String userName,
+			@RequestParam(name="password",required=true) String password,
+			HttpServletResponse res,
+			Model model) {
+		try {
+			User user = new User();
+			user.setUserName(userName);
+			user.setPassword(password);
+			
+			Authenticator.login(user);
+			
+			Cookie userNameCookie = new Cookie(USER_NAME_COOKKIE_NAME,user.getUserName());
+			Cookie passwordCookie = new Cookie(PASSWORD_COOKKIE_NAME,user.getPassword());
+			
+			userNameCookie.setMaxAge(COOKIES_LIFE_TIME);
+			passwordCookie.setMaxAge(COOKIES_LIFE_TIME);
+			
+			res.addCookie(userNameCookie);
+			res.addCookie(passwordCookie);
+			
+			return CHAT_ROOM_PATH;
+		}catch(Exception e) {
+			model.addAttribute("error_message", e.getMessage());
+			return AUTHENTICATION_PATH;
+		}
+	}
+	
+}
--- chat-application/src/main/java/main/generals/authentication/exceptions/AuthenticatorController.java
+++
@@ -1,8 +0,0 @@
-package main.generals.authentication.exceptions;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class AuthenticatorController {
-	
-}
